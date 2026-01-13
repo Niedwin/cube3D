3137dab2126ddf537ac7d96b1ef52898918a3ce9
Kerloye Guillemin <kguillem@c1r2p6.42perpignan.fr>
2026-01-13 14:19:07 +0100

    made functions to initialize the parsers struct as NULL

--- src/initalize.c
+++ src/initalize.c
@@ -6,7 +6,7 @@
 /*   By: guviure <guviure@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 19:34:00 by guviure           #+#    #+#             */
-/*   Updated: 2025/12/10 19:34:43 by guviure          ###   ########.fr       */
+/*   Updated: 2026/01/13 14:16:20 by kguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,9 +53,40 @@ int	check_args_n_fd(int argc, char **argv)
 	return (fd);
 }
 
-//TODO
-
-int	initialize_struct(t_header *header)
+void	null_colors(t_header *header)
 {
-	//initialize all fields to NULL or -1
+	header->ceiling[0] = NULL;
+	header->ceiling[1] = NULL;
+	header->ceiling[2] = NULL;
+	header->ceiling[3] = NULL;
+	header->floor[0] = NULL;
+	header->floor[1] = NULL;
+	header->floor[2] = NULL;
+	header->floor[3] = NULL;
+	return	;
+}
+
+void	null_textures(t_header *header)
+{
+	header->north = NULL;
+	header->south = NULL;
+	header->east = NULL;
+	header->west = NULL;
+	return ;
+}
+
+void	null_map(t_header *header)
+{
+	header->map = NULL;
+	header->pos[0] = 0;
+	header->pos[1] = 0;
+	return ;
+}
+
+void	initialize_struct(t_header *header)//initialize all fields to NULL
+{
+	null_colors(header);
+	null_textures(header);
+	null_map(header);
+	return ;
 }
